2b0e36caeebd4f9b1e2a52094c385e137b3eb62c
Fang Han <hanfa@umich.edu>
2019-01-23 15:42:17 -0500

    cprofiler results for face tracking

--- RemoteFaceClassifier/Client/__init__.py
+++ RemoteFaceClassifier/Client/__init__.py
@@ -3,7 +3,7 @@ CLIENT_FRAME_INTERVAL = 1000 # time between sending each frame (ms)
 CLASSIFY_THRESHOLD = 0.7 # label a face when the confidence is greater than this
 
 # Front end configurations
-SHOW_GUI = True
+SHOW_GUI = False
 WINDOW_NAME = "Face tracking & classification demo\t Thres={}".format(CLASSIFY_THRESHOLD)
 LABEL_COLOR = (255, 255, 0)
 FACE_RECT_COLORS = {
